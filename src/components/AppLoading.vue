48fa05597088b0489e90c3be41ff29bbb9dff43e
Nadezhda Rachkova <nadezda.rachkova@mail.ru>
2023-04-03 17:51:32 +0300

    Лодер. Создан компонент

---
+++ src/components/AppLoading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="loading" class="loading"></div>
+</template>
+
+<script>
+export default {
+  name: "AppLoading",
+  props: {
+    loading: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
