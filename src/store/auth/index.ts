942bab5c774111a57e42a74c3e747a64733adbab
Надежда Рачкова <nadezda.rachkova@mail.ru>
2023-05-12 15:53:01 +0300

    Авторизация. В стор добавлен user

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,13 +1,17 @@
 import {Module} from "vuex";
 import {RootState} from "@/store/types";
-import {authState} from './types'
+import {authState, fbPayload} from './types'
 import http from "@/http";
 import {AuthInfo} from "@/typings";
+import {getJWTPayload, setTokens} from "@/utils/token"
 
 
 export const auth: Module<authState, RootState> = {
   namespaced: true,
-  state: () => ({ token: localStorage.getItem('jwt-token') }),
+  state: () => ({
+    token: localStorage.getItem('jwt-token'),
+    user: null
+  }),
   getters: {
     token: (state) => state.token,
     isAuth: (_, getters) => !!getters.token
@@ -20,6 +24,18 @@ export const auth: Module<authState, RootState> = {
     removeToken(state) {
       localStorage.removeItem('jwt-token')
       state.token = null
+    },
+    setUser(state, fbPayload :fbPayload) {
+      state.user = {
+        email: fbPayload.email,
+        email_verified: fbPayload.email_verified,
+        auth_time: fbPayload.auth_time,
+        user_id: fbPayload.user_id,
+        exp_time:  fbPayload.exp // истекает
+      }
+    },
+    removeUser (state) {
+      state.user = null
     }
   },
   actions: {
@@ -29,7 +45,13 @@ export const auth: Module<authState, RootState> = {
         const authUrl = process.env.VUE_APP_FB_AUTH_URL
         const url = `${authUrl}/accounts:signInWithPassword?key=${apiKey}`
         const {data} = await http.post(url, {...authInfo, returnSecureToken: true})
-        commit('setToken', data.idToken)
+        const accessToken :string = data.idToken
+        const fbPayload: fbPayload = getJWTPayload(accessToken)
+        console.log(data, fbPayload)
+
+        commit('setToken', accessToken)
+        commit('setUser', fbPayload)
+
         return { result: true, data }
       } catch (e) {
         return { result: false, data: e.response.data }
@@ -37,6 +59,7 @@ export const auth: Module<authState, RootState> = {
     },
     logout({commit}) {
       commit('removeToken')
+      commit('removeUser')
     },
     async singUp({commit, dispatch}, singUpInfo: AuthInfo) {
       try {
@@ -44,6 +67,7 @@ export const auth: Module<authState, RootState> = {
         const authUrl = process.env.VUE_APP_FB_AUTH_URL
         const url = `${authUrl}/accounts:signUp?key=${apiKey}`
         const {data} = await http.post(url, {...singUpInfo, returnSecureToken: true})
+        setTokens(data.accessToken);
         commit('setToken', data.idToken)
         return  { result: true, data }
       } catch (e) {
--- src/store/auth/types.ts
+++ src/store/auth/types.ts
@@ -1,3 +1,16 @@
 export interface authState {
-  token: string | null
+  token: string | null,
+  user : User | null
 }
+
+export interface User {
+  user_id: string,
+  email: string,
+  email_verified: boolean,
+  auth_time: number,
+  exp_time: number
+}
+
+//todo: уточнить у firebase точное определение jwt-token payload
+//todo: избавиться от any
+export type fbPayload = Object | any
