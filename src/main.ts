5dd0b98dabe3740b210d7a8a7fbda87aa9e68bbf
Надежда Рачкова <nadezda.rachkova@mail.ru>
2023-05-19 15:09:37 +0300

    Аутентификация. Загрузка профиля перед загрузкой приложения

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,36 @@ import router from './router'
 import store from './store'
 import authHttp from "@/authHttp";
 
-try {
-  const apiKey = process.env.VUE_APP_FB_API_KEY
-  const secureUrl = process.env.VUE_APP_FB_SECURE_URL
-  const refreshToken = localStorage.getItem('refresh-token')
-  if (refreshToken) {
-    const url = `${secureUrl}/token?key=${apiKey}`
-    const payload = { refresh_token: refreshToken,  grant_type: "refresh_token" }
-    authHttp.post(url, payload).then((response)=> {
-      const newAccessToken = response.data.id_token
-      const newRefreshToken = response.data.refresh_token
-      store.commit('auth/setAccessToken',newAccessToken)
-      store.commit('auth/setRefreshToken',newRefreshToken)
-    })
-  }
-} catch(e) {
-  console.log(e)
-} finally {
+function loadApp() {
   createApp(App).use(store).use(router).mount('#app')
 }
+
+
+const apiKey = process.env.VUE_APP_FB_API_KEY
+const secureUrl = process.env.VUE_APP_FB_SECURE_URL
+const refreshToken = localStorage.getItem('refresh-token')
+if (refreshToken) {
+  const url = `${secureUrl}/token?key=${apiKey}`
+  const payload = {refresh_token: refreshToken, grant_type: "refresh_token"}
+  authHttp.post(url, payload)
+    .then(
+      (response) => {
+        const newAccessToken = response.data.id_token
+        const newRefreshToken = response.data.refresh_token
+        store.commit('auth/setAccessToken', newAccessToken)
+        store.commit('auth/setRefreshToken', newRefreshToken)
+        store.dispatch('profile/fetchProfile')
+      },
+      ()=> {
+        store.commit('auth/removeAccessToken')
+        store.commit('auth/removeRefreshToken')
+      }
+    )
+    .finally(() => {
+      loadApp()
+    }
+  )
+} else {
+  loadApp()
+}
+
