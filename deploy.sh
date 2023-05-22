6240f5091dd9446fca4cc8d5e64295cf5a2b0d69
Надежда Рачкова <nadezda.rachkova@mail.ru>
2023-05-22 17:24:43 +0300

    Деплой

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'Деплой.'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:hilaratas/hilaratas.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:hilaratas/ldv-spa.git master:gh-pages
+
+cd -
