e488f1c3968d4d5aacd68b43831c2ab3fca1c6ca
Надежда Рачкова <nadezda.rachkova@mail.ru>
2023-05-30 12:00:17 +0300

    Каталог. Добавлена навигация для админа

--- src/views/catalog/CatalogSections.vue
+++ src/views/catalog/CatalogSections.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="services">
-    <div v-if="!catalogSections" class="title title--h3">
-      На данный момент каталог пуст
+  <div v-if="!catalogSections" class="title title--h3">
+    На данный момент каталог пуст
+    <br>
+  </div>
+  <div v-if="isAuth" style="padding-bottom: 15px">
+    <div class="row row--6px">
+      <div class="col-auto"><router-link class="button" to="/catalog/add">Добавить новый раздел</router-link></div>
+      <div class="col-auto"><router-link class="button" to="/catalog/product/add">Добавить новый продукт</router-link></div>
     </div>
-    <div v-else class="services__zz row row--30px row--small-phone-15px row--mt60px">
+  </div>
+  <div v-if="catalogSections" class="services">
+    <div class="services__zz row row--30px row--small-phone-15px row--mt60px">
       <div v-for="(cat,key) in catalogSections" class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3" :key="cat.hru">
         <router-link :to="'/catalog/' + key" class="services__item" >
           <div class="services__img-holder aos-init aos-animate" data-aos="fade-left">
@@ -30,7 +37,11 @@ export default {
   setup() {
     const store = useStore()
     const catalogSections = store.getters['catalog/catalogSections']
+    const isAuth = store.getters['auth/isAuth']
+    console.log(isAuth)
+
     return {
+      isAuth,
       catalogSections
     }
   }
